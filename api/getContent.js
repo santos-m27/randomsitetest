acbae8a645524409d54523ce92be67fdb54a8a10
Santos <santosm2710@gmail.com>
2025-04-08 20:59:46 +0100

    a

--- api/getContent.js
+++ api/getContent.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
     // Simulating fetching data from an external source (like a remote file or DB)
     const content = {
-      title: "Your Name",
+      title: "Yaaaour Name",
       description: "Developer • Creator • Innovator",
       projects: ["Project 1", "Project 2", "Project 3"]
     };
